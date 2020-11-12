7cbc8a3da5dcff614d63044e04b9d1257da61915
Jean M. Lescure <jeanmlescure@gmail.com>
2020-11-11 19:00:33 -0600

    fix: typo on sh command

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ EOF
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
 sh -c "yarn" \
-&& -c "${NODE_ENV_PREPEND} yarn build" \
+&& sh -c "${NODE_ENV_PREPEND} yarn build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
               --no-progress \
