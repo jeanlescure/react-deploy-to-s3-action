f2c9cb5a8a60a00d03b12a99bc6e79d0ffa330dc
Jon Belelieu <jbelelieu@users.noreply.github.com>
2022-04-21 09:53:09 -0400

    Remove yarn reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,10 @@ NODE_ENV_PREPEND="NODE_ENV=${NODE_ENV:-production}"
 
 # Override yarn in favor of npm
 if [ "$USE_NPM_OVER_YARN" = true ]; then
+  PACKAGE_MANAGER="npm"
   PACKAGE_MANAGER_COMMAND="${NODE_ENV_PREPEND} npm run build"
 else
+  PACKAGE_MANAGER="yarn"
   PACKAGE_MANAGER_COMMAND="${NODE_ENV_PREPEND} yarn build"
 fi
 
@@ -51,7 +53,7 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "yarn" \
+sh -c "${PACKAGE_MANAGER}" \
 && sh -c "${PACKAGE_MANAGER_COMMAND}" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
